7b242c7da3745d834e7fc6695313cf0b93db5c22
Alexander Walther <mail@alexplus.de>
2024-08-26 10:25:30 +0200

    setup verbessert

--- install.php
+++ install.php
@@ -1,3 +1,10 @@
 <?php
 
 $addon = rex_addon::get('schwooops');
+
+if(rex_config::get('schwooops', 'name', '') == '') {
+    rex_config::set('schwooops', 'name', rex::getServerName());
+}
+if(rex_config::get('schwooops', 'email', '') == '') {
+    rex_config::set('schwooops', 'email', rex::getErrorEmail());
+}
